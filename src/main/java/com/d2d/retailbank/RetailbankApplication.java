15028ee31f3f66c62252e14c9382be4be4a60b0e
amitdatta <the.amisoft@gmail.com>
2022-01-21 09:35:52 +0530

    integrated with service registry

--- src/main/java/com/d2d/retailbank/RetailbankApplication.java
+++ src/main/java/com/d2d/retailbank/RetailbankApplication.java
@@ -2,7 +2,9 @@ package com.d2d.retailbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class RetailbankApplication {
 
